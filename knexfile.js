a2db28f0016aa96655bf7f21b72da9caa158124d
joaovitoralvarenga <ggquennbr@gmail.com>
2025-10-08 15:57:34 -0300

    alt:atualizção

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || {
+      host: 'localhost',
+      port: 5432,
+      user: 'postgres',
+      password: 'postgres',
+      database: 'teste_db'
+    },
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  },
+  
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  }
+};
